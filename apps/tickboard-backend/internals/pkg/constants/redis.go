06510d9c451a9fa039b57b97d676e5c02671528f
halimdotnet <halimdotnet@gmail.com>
2026-05-15 11:34:37 +0700

    update
    Signed-off-by: halimdotnet <halimdotnet@gmail.com>

---
+++ apps/tickboard-backend/internals/pkg/constants/redis.go
@@ -0,0 +1,5 @@
+package constants
+
+const (
+	RedisPrefixRateLimit = "ratelimit:"
+)
---
+++ apps/tickboard-backend/internals/pkg/utilities/uslice/uslice.go
@@ -0,0 +1,38 @@
+package uslice
+
+import "math/rand"
+
+// Contains checks if the slice contains the item.
+func Contains[T comparable](slice []T, item T) bool {
+	for _, v := range slice {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
+
+// IndexOf returns the index of the first occurrence of the item in the slice.
+func IndexOf[T comparable](slice []T, item T) int {
+	for i, v := range slice {
+		if v == item {
+			return i
+		}
+	}
+	return -1
+}
+
+// Reverse reverses the order of the elements in the slice.
+func Reverse[T any](slice []T) {
+	for i, j := 0, len(slice)-1; i < j; i, j = i+1, j-1 {
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
+
+// Shuffle randomly permutes the elements of the slice.
+func Shuffle[T any](slice []T) {
+	for i := len(slice) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
